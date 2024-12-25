314e130d0584f62b112587ca1607cd8d4d951a9c
vaish-rzz <vaisakhhhh33@gmail.com>
2024-12-25 20:58:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('smileButton').addEventListener('click', () => {
+    const message = document.getElementById('message');
+    message.textContent = "Your smile makes the world brighter and my life brighter! 🌈";
+});
